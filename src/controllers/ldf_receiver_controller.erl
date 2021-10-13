2c63ca3b96330185752f1113d5a422baf6f27f0c
Daniel Widgren <daniel.widgren@gmail.com>
2021-10-13 14:03:17 +0200

    added a config where chatli base path is. Also added, when ldf saves message to db it will enter a full path so it is easier to get messages.

--- src/controllers/ldf_receiver_controller.erl
+++ src/controllers/ldf_receiver_controller.erl
@@ -5,7 +5,14 @@
 
 message(#{req := #{method := <<"POST">>},
         json := #{<<"id">> := MessageId} = Json}) ->
-    ok = ldf_db:add_message(encode(Json), MessageId),
+    Message = case Json of
+                  #{<<"payload">> := #{<<"url">> := Url} = Payload} ->
+                        {ok, ChatliPath} = application:get_env(ldf, chatli_path),
+                        Payload2 = maps:update(<<"url">>, <<ChatliPath/binary, Url/binary>>, Payload),
+                        maps:update(<<"payload">>, Payload2, Json);
+                    _ -> Json
+              end,
+    ok = ldf_db:add_message(encode(Message), MessageId),
     {status, 200};
 message(#{req := #{method := <<"GET">>}}) ->
     {ok, List} = ldf_db:get_messages(),
